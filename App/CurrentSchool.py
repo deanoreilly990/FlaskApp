076f55483c74aeacf45e49e7322e3dfb99a29bf4
dor <c13469208@mydit.ie>
2017-03-05 17:57:12 +0000

    Compare Charts No.9

--- App/CurrentSchool.py
+++ App/CurrentSchool.py
@@ -1,14 +1,46 @@
-def logdata(value1, value2):
-
-    import pandas as pd
+def get_daft():
     from pymongo import MongoClient
+    client = MongoClient('localhost:27017')
+    db = client.rentalData
+    return db
+def getdate():
+    import datetime
+    Months = {
+        'Jan': 1,
+        'Feb': 2,
+        'Mar': 3,
+        'Apr': 4,
+        'May': 5,
+        'Jun': 6,
+        'Jul': 7,
+        'Aug': 8,
+        'Sep': 9,
+        'Oct': 10,
+        'Nov': 11,
+        'Dec': 12
+    }
+    month = datetime.datetime.now()
+    month = month.strftime("%b")
+    date = Months[month]
+    date = date - 1
+    if date == 2:
+        enddate = "-28"
+    elif date in [4, 6, 11, 9]:
+        enddate = "-30"
+    else:
+        enddate = "-31"
+    return date,enddate
+def get_db():
+    from pymongo import MongoClient
+    client = MongoClient('localhost:27017')
+    db=client.HistData
+    return db
+def logdata(value1, value2):
+    from pymongo import MongoClient
+    import pandas as pd
     global data
     global data
-    def get_daft():
-        client = MongoClient('localhost:27017')
-        db=client.HistData
-        return db
-    db = get_daft()
+    db = get_db()
     data = db.primaryschools.find({},{'_id':0})
     data = pd.DataFrame(list(data))
     data['PC'] = data['PC'].str.lower()
@@ -18,6 +50,8 @@ def logdata(value1, value2):
     bedroomsLastMonth(value2,'v2')
     averagePerRoom(value1,'v1')
     averagePerRoom(value2,'v2')
+    rentalCharts(value1,'v1')
+    rentalCharts(value2,'v2')
     value1 = 'dublin '+str(value1)
     value2 = 'dublin '+str(value2)
     v1.append(getdata(value1))
@@ -73,10 +107,6 @@ def plot(name,boys,girls,religions,religionsCount,v):
 def bedroomsLastMonth(value1,v1):
     import pandas as pd
     from pymongo import MongoClient
-    def get_daft():
-        client = MongoClient('localhost:27017')
-        db=client.rentalData
-        return db
     import plotly.plotly as py
     import plotly.graph_objs as go
     import plotly
@@ -119,40 +149,11 @@ def averagePerRoom(pc,version):
     import calendar
     py.sign_in('deanoreilly990', 'WgnPSOLqPlZ5uXFu6WKx')
 
-    def get_daft():
-        client = MongoClient('localhost:27017')
-        db = client.rentalData
-        return db
-
     db = get_daft()
     data = db.sale.find({}, {'_id': 0})
     data = pd.DataFrame(list(data))
     data = data.dropna()
-    Months = {
-        'Jan': 1,
-        'Feb': 2,
-        'Mar': 3,
-        'Apr': 4,
-        'May': 5,
-        'Jun': 6,
-        'Jul': 7,
-        'Aug': 8,
-        'Sep': 9,
-        'Oct': 10,
-        'Nov': 11,
-        'Dec': 12
-    }
-
-    month = datetime.datetime.now()
-    month = month.strftime("%b")
-    date = Months[month]
-    date = date - 1
-    if date == 2:
-        enddate = "-28"
-    elif date in [4, 6, 11, 9]:
-        enddate = "-30"
-    else:
-        enddate = "-31"
+    date,enddate = getdate()
     str1 = "2017-0" + str(date) + "-01"
     str2 = "2017-0" + str(date) + enddate
     data = data[data['Post Code'] == pc]
@@ -206,3 +207,178 @@ def averagePerRoom(pc,version):
     data = [trace]
     fig = go.Figure(data=data, layout=layout)
     plotly.offline.plot(fig, filename='/home/dor/FlaskApp/App/static/images/2017/V4-' + version + '.html', show_link=False,auto_open=False)
+
+
+
+
+
+def rentalCharts(pc,v1):
+    import pandas as pd
+    from pymongo import MongoClient
+    import plotly.plotly as py
+    import plotly.graph_objs as go
+    import plotly
+    import datetime
+    import calendar
+    py.sign_in('deanoreilly990', 'WgnPSOLqPlZ5uXFu6WKx')
+    db= get_daft()
+    data = db.rentFinal.find({},{'_id':0})
+    data = pd.DataFrame(list(data))
+    data = data.dropna()
+    date,enddate = getdate()
+    str1 = "2017-0" + str(date) + "-01"
+    str2 = "2017-0" + str(date) + enddate
+    data = data[data['Post Code'] == pc]
+    data['Date Entered'] = pd.to_datetime(data['Date Entered'])
+    data = data[data["Date Entered"].isin(pd.date_range(str1, str2))]
+    typecount = [0, 0, 0]
+    studioprice = 0
+    houseprice = [0, 0, 0, 0]
+    housetype = [0, 0, 0, 0]
+    apartmentprice = [0, 0, 0, 0]
+    apartmenttype = [0, 0, 0, 0]
+    for index, row in data.iterrows():
+        price = row[4]
+        price = price.encode('ascii', 'ignore')
+        try:
+            price = price.replace(',', '')
+        except:
+            pass
+        try:
+            price = float(price)
+        except:
+            continue
+        if 'Studio' in row[5]:
+            typecount[0] = typecount[0] + 1
+            studioprice = studioprice + price
+        if 'House' in row[5]:
+            typecount[1] = typecount[1] + 1
+            if '2' in row[1]:
+                housetype[0] = housetype[0] + 1
+                houseprice[0] = houseprice[0] + price
+            elif '3' in row[1]:
+                housetype[1] = housetype[1] + 1
+                houseprice[1] = houseprice[1] + price
+            elif '4' in row[1]:
+                housetype[2] = housetype[2] + 1
+                houseprice[2] = houseprice[2] + price
+            elif '5' in row[1]:
+                housetype[3] = housetype[3] + 1
+                houseprice[3] = houseprice[3] + price
+        elif 'Apartment' in row[5]:
+            typecount[2] = typecount[2] + 1
+            if '2' in row[1]:
+                apartmenttype[0] = apartmenttype[0] + 1
+                apartmentprice[0] = apartmentprice[0] + price
+            elif '3' in row[1]:
+                apartmenttype[1] = apartmenttype[1] + 1
+                apartmentprice[1] = apartmentprice[1] + price
+            elif '4' in row[1]:
+                apartmenttype[2] = apartmenttype[2] + 1
+                apartmentprice[2] = apartmentprice[2] + price
+            elif '5' in row[1]:
+                apartmenttype[3] = apartmenttype[3] + 1
+                apartmentprice[3] = apartmentprice[3] + price
+    try:
+        studioprice = studioprice / typecount[0]
+    except:
+        pass
+    houseaverage = []
+    apartmentaverage = []
+    apartmentaverage.append(studioprice)
+
+    for i in range(4):
+        try:
+            averageh = houseprice[i] /housetype[i]
+            houseaverage.append(averageh)
+            averageA = apartmentprice[i]/apartmenttype[i]
+            apartmentaverage.append(averageA)
+        except:
+            pass
+    f = lambda x:round(x,2)
+    apartmentaverage = map(f,apartmentaverage)
+    houseaverage = map(f,houseaverage)
+
+    fig = {
+                'data': [{'labels': [ 'Studio - 1bed','Houses','Apartments'],
+                              'values': typecount,
+                              'type': 'pie'}],
+                    'layout': {'title': 'Ratio of properties availble last month ',
+                              'autosize':False,
+                              'width':500,
+                              'height':400,
+                              'margin':{'l':10,'r':10,'b':50,'t':50,'pad':4}}
+                     }
+    plotly.offline.plot(fig,filename='/home/dor/FlaskApp/App/static/images/2017/V5-'+v1+'.html',show_link=False,auto_open=False)
+
+    trace = go.Bar(
+            x=['1 Bed-Studio','2 Bed','3 Bed','4 Bed','5 Bed'],
+            y=apartmentaverage
+        )
+    layout = go.Layout(
+        title='Average Price of Apartment by room type in  : ' + calendar.month_name[date],
+        yaxis=dict(
+            title='Average Price Per Month'),
+        xaxis=dict(
+            title='No.Beds'),
+        autosize=False,
+        width=500,
+        height=400,
+        margin=go.Margin(
+            l=80,
+            r=10,
+            b=90,
+            t=50,
+            pad=10
+        ),
+    )
+    data = [trace]
+    fig = go.Figure(data=data, layout=layout)
+    plotly.offline.plot(fig, filename='/home/dor/FlaskApp/App/static/images/2017/V6-' + v1 + '.html', show_link=False,auto_open=False)
+    trace = go.Bar(
+            x=['2 Bed','3 Bed','4 Bed','5 Bed'],
+            y=houseaverage
+        )
+    layout = go.Layout(
+        title='Average Price of Houses by room type in: ' + calendar.month_name[date],
+        yaxis=dict(
+            title='Average Price Per Month'),
+        xaxis=dict(
+            title='No.Beds'),
+        autosize=False,
+        width=500,
+        height=400,
+        margin=go.Margin(
+            l=80,
+            r=10,
+            b=90,
+            t=50,
+            pad=10
+        ),
+    )
+    data = [trace]
+    fig = go.Figure(data=data, layout=layout)
+    plotly.offline.plot(fig, filename='/home/dor/FlaskApp/App/static/images/2017/V7-' + v1 + '.html', show_link=False,auto_open=False)
+    apartmenttype.append(typecount[0])
+    fig = {
+                'data': [{'labels': ['2 Bed','3 Bed','4 Bed','5 Bed','Studio - 1 bed'],
+                              'values': apartmenttype,
+                              'type': 'pie'}],
+                    'layout': {'title': 'Ratio of type of Apartments Available ',
+                              'autosize':False,
+                              'width':500,
+                              'height':400,
+                              'margin':{'l':10,'r':20,'b':50,'t':50,'pad':4}}
+                     }
+    plotly.offline.plot(fig,filename='/home/dor/FlaskApp/App/static/images/2017/V8-'+v1+'.html',show_link=False,auto_open=False)
+    fig = {
+                'data': [{'labels': ['2 Bed','3 Bed','4 Bed','5 Bed'],
+                              'values': housetype,
+                              'type': 'pie'}],
+                    'layout': {'title': 'Ratio of type of Houes Available ',
+                              'autosize':False,
+                              'width':500,
+                              'height':400,
+                              'margin':{'l':10,'r':20,'b':50,'t':50,'pad':4}}
+                     }
+    plotly.offline.plot(fig,filename='/home/dor/FlaskApp/App/static/images/2017/V9-'+v1+'.html',show_link=False,auto_open=False)
